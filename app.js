cc20f28b220dcc097e5b22ed6483089520b32024
Dale Rogers <dalerogers@Dales-Mac-mini.modem>
2023-05-13 16:48:49 +1000

    Syncing repository with latest changes

---
+++ app.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // registration logic here
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button type="submit">Register</Button>
+    </form>
+  );
+};
+
+export default Register;
---
+++ register.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { register } from './firebase';
+
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const success = await register({ email, password });
+    if (success) {
+      alert('Registration successful!');
+    } else {
+      alert('Registration failed.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register;
