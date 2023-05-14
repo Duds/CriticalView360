5aeaaab9e6f92af03d347defb8a2894e52607b25
Dale Rogers <contact@dalerogers.com.au>
2023-05-14 14:48:59 +1000

    Working basic setup. MongoDB Running, Mongoose connected, React running. Server running.

---
+++ models/bowTieCards.js
@@ -0,0 +1,33 @@
+// bowTieCard.js
+const mongoose = require('mongoose');
+
+const bowTieCardSchema = new mongoose.Schema({
+  hazard: { type: String, required: true },
+  causes: [
+    {
+      text: { type: String, required: true },
+      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    }
+  ],
+  preventativeControls: [
+    {
+      text: { type: String, required: true },
+      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    }
+  ],
+  mitigatingControls: [
+    {
+      text: { type: String, required: true },
+      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    }
+  ],
+  consequences: [
+    {
+      text: { type: String, required: true },
+      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    }
+  ],
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+});
+
+module.exports = mongoose.model('BowTieCard', bowTieCardSchema);
---
+++ models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    // any additional properties you want to store for users
+  });
+  
